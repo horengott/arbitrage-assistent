ce7401bdd4041ce65223ab693768a325fa5bbb66
horengott <imarsenium@gmail.com>
2026-01-31 23:08:06 +0100

    added liquidity check, vwap calculation

--- exchange/fetcher.py
+++ exchange/fetcher.py
@@ -2,119 +2,88 @@ import ccxt.async_support as ccxt
 import asyncio  
 import time
 
-# get list of exchanges:
 
-def get_exchanges():
-    return ccxt.exchanges
+# checking liquidity:
 
-
-# getting the best ask price:
-
-async def best_ask(token):
+async def calculate_vwap(order_book_side, target_amount_usdt):
+        remaining_usdt = target_amount_usdt
+        total_tokens = 0
     
-        best_ask_price = {
-            'price': 100000000,
-            'amount': 0,
-            'exchange': None
-        }
-
-        print("starting...")
-        bolsas = 0
-
-        for exchange in ['binance', 'bybit', 'okx', 'kucoin', 'gateio']:
-            print(f"checking: {exchange}")
-            ex = getattr(ccxt, exchange)
-            current_exchange = ex({'timeout': 3000, 'enableRateLimit': True})
+        for i in range(len(order_book_side)):
+            level_price = order_book_side[i][0]
+            level_volume = order_book_side[i][1]
         
-            try:
-
-                await current_exchange.load_markets()
-
-                if token in current_exchange.symbols:
-                    orderbook = await current_exchange.fetch_order_book(token, limit=20)
-
-                    print(f"se va a ver: {orderbook['asks'][0][0]} de {exchange}")
-
-                    if orderbook['asks'][0][0] < best_ask_price['price']:
-                        best_ask_price['price'] = orderbook['asks'][0][0]
-                        best_ask_price['amount'] = orderbook['asks'][0][1]
-                        best_ask_price['exchange'] = exchange
-                        print(f" lo que entra en el if: {best_ask_price}")
-                        bolsas += 1
-                    else:
-                        continue
-                else:
-                    continue
-
-
-
-            except ccxt.BaseError as e:
-                print(f"error de ccxt: {exchange}")
-                continue
-
-            except Exception as e:
-                print(f"error desconocido: {exchange}")
-                continue
-
-            finally:
-                await current_exchange.close()
+            level_total_cost = level_price * level_volume
         
-        print(f"el mejor precio es en {best_ask_price['exchange']}")
-        return f"el mejor precio es en {best_ask_price['exchange']}"
-        
+            if remaining_usdt <= level_total_cost:
+                bought_tokens = remaining_usdt / level_price
+                total_tokens += bought_tokens
+                remaining_usdt = 0
+                break 
+            else:
+                total_tokens += level_volume
+                remaining_usdt -= level_total_cost
+            
+        if remaining_usdt > 0:
+            return None 
 
-# getting the best ask price:
+        return target_amount_usdt / total_tokens
 
-async def best_bid(token):
+
+async def get_liquid_exchanges(exchanges, token_symbol, user_balance_usdt):
+        symbol = f"{token_symbol}/USDT"
     
-        best_bid_price = {
-            'price': 0,
-            'amount': 0,
-            'exchange': None
-        }
-
-        print("starting...")
-        bolsas = 0
-
-        for exchange in ['binance', 'bybit', 'okx', 'kucoin', 'gateio']:
-            print(f"checking: {exchange}")
-            ex = getattr(ccxt, exchange)
-            current_exchange = ex({'timeout': 3000, 'enableRateLimit': True})
-        
+        for exchange in exchanges:
             try:
-
-                await current_exchange.load_markets()
-
-                if token in current_exchange.symbols:
-                    orderbook = await current_exchange.fetch_order_book(token, limit=20)
-
-                    print(f"se va a ver: {orderbook['bids'][0][0]} de {exchange}")
-
-                    if orderbook['bids'][0][0] > best_bid_price['price']:
-                        best_bid_price['price'] = orderbook['bids'][0][0]
-                        best_bid_price['amount'] = orderbook['bids'][0][1]
-                        best_bid_price['exchange'] = exchange
-                        print(f" lo que entra en el if: {best_bid_price}")
-                        bolsas += 1
-                    else:
-                        continue
-                else:
-                    continue
-
-
-
-            except ccxt.BaseError as e:
-                print(f"error de ccxt: {exchange}")
-                continue
-
+                order_book = await exchange.fetch_order_book(symbol, limit=50)
+            
+                buy_price = await calculate_vwap(order_book['asks'], user_balance_usdt)
+            
+                sell_price =  await calculate_vwap(order_book['bids'], user_balance_usdt)
+            
+                if buy_price and sell_price:
+                    yield {
+                        'exchange': exchange.id,
+                        'buy_price': buy_price,  
+                        'sell_price': sell_price, 
+                        'original_ask': order_book['asks'][0][0] 
+                    }
+                
             except Exception as e:
-                print(f"error desconocido: {exchange}")
                 continue
 
-            finally:
-                await current_exchange.close()
-        
-        print(f"el mejor precio es en {best_bid_price['exchange']}")
-        return f"el mejor precio es en {best_bid_price['exchange']}"
 
-asyncio.run(best_bid("ETH/USDT"))
+
+# getting the best ask/bid price:
+
+async def find_best_route(token_symbol, user_balance_usdt):
+    exchange_names = ['binance', 'bybit', 'okx', 'kucoin', 'gateio']
+    
+    exchanges = [getattr(ccxt, name)({'enableRateLimit': True}) for name in exchange_names]
+
+    best_buy = {'exchange': None, 'price': float('inf')}
+    best_sell = {'exchange': None, 'price': 0}
+
+    print("searching the best price")
+
+    async for market_data in get_liquid_exchanges(exchanges, token_symbol, user_balance_usdt):
+        print(f"exchange {market_data['exchange']} is aproved by liquidity")
+        print(f"real buy price: {market_data['buy_price']}")
+
+        if market_data['buy_price'] < best_buy['price']:
+            best_buy['exchange'] = market_data['exchange']
+            best_buy['price'] = market_data['buy_price']
+
+        if market_data['sell_price'] > best_sell['price']:
+            best_sell['exchange'] = market_data['exchange']
+            best_sell['price'] = market_data['sell_price']
+
+    if best_buy['exchange'] and best_sell['exchange']:
+        print(f"buying {best_buy['exchange']} by {best_buy['price']}")
+        print(f"selling {best_sell['exchange']} by {best_sell['price']}")
+        return best_buy, best_sell
+    else:
+        print("there are not liquidit routes")
+        return None, None
+        
+
